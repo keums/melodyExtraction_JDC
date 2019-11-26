0e46f062d48e9ddb52f3509b6be371798c0cca06
Sangeun Kum <keums@kaist.ac.kr>
2019-11-26 19:48:56 +0900

    Update melodyExtraction_JDC.py

--- melodyExtraction_JDC.py
+++ melodyExtraction_JDC.py
@@ -1,64 +1,78 @@
 
 # -*- coding: utf-8 -*-
-"""
-Created on Fri Jan 19 00:04:22 2019
-
-@author: keums
-"""
 
 import os
 import sys
-# os.environ['CUDA_DEVICE_ORDER']='PCI_BUS_ID'
-# os.environ['CUDA_VISIBLE_DEVICES']=''
+import argparse
 import numpy as np
 from keras.utils import multi_gpu_model
 import matplotlib.pyplot as plt
 from scipy.signal import medfilt
 from model import *
 from featureExtraction import *
+import glob
 
 class Options(object):
     def __init__(self):
         self.num_spec = 513
         self.input_size = 31#115
         self.batch_size = 64#64
-
-        self.use_multi_gpu = 1
         self.resolution = 16
-        self.thres_v = 0.5
-        self.figureON = True
+        self.figureON = False
 
 options = Options()
 
-def main(file_name):
+def main(filepath,output_dir,gpu_index):
+
+
+    if gpu_index is not None:
+        os.environ['CUDA_DEVICE_ORDER'] = 'PCI_BUS_ID'
+        os.environ['CUDA_VISIBLE_DEVICES'] = str(gpu_index)
+    
+
     pitch_range = np.arange(38, 83 + 1.0/options.resolution, 1.0/options.resolution)
     pitch_range = np.concatenate([np.zeros(1), pitch_range])
 
-    '''  Extracting features '''
-    X_test, X_spec = spec_extraction(file_name=file_name, win_size=options.input_size)
+    '''  Features extraction'''
+    X_test, X_spec = spec_extraction(file_name=filepath, win_size=options.input_size)
 
 
-    '''  Prediction of melody '''
+    '''  melody predict'''
     model = melody_ResNet_joint_add(options)
-    model.load_weights('./weights/ResNet_joint_add_L(CE_G).hdf5')
+    # model.load_weights('./weights/ResNet_joint_add_L(CE_G).hdf5')
+    # model = melody_ResNet_joint_add2(options)
+    model.load_weights(
+        './weights/ResNet_joint_add_L(CE_G)_r16_t3_singleGPU.hdf5')
     y_predict = model.predict(X_test, batch_size=options.batch_size, verbose=1)
 
     num_total = y_predict[0].shape[0] * y_predict[0].shape[1]
     # y_predict_v = np.reshape(y_predict[1], (num_total, 2))
 
     est_pitch = np.zeros(num_total)
-    index_predict = np.zeros(num_total)
+    # index_predict = np.zeros(num_total)
 
     y_predict = np.reshape(y_predict[0], (num_total, y_predict[0].shape[2]))  # origin
     for i in range(y_predict.shape[0]):
-        index_predict[i] = np.argmax(y_predict[i, :])
-        pitch_MIDI = pitch_range[np.int32(index_predict[i])]
+        idxMaxPitch = np.argsort(y_predict[i, :])[-1]
+        if idxMaxPitch == 0:
+            idxMaxPitch = np.argsort(y_predict[i, :])[-4]
+        pitch_MIDI = pitch_range[np.int32(idxMaxPitch)]
         if pitch_MIDI >= 38 and pitch_MIDI <= 83:
             est_pitch[i] = 2 ** ((pitch_MIDI - 69) / 12.) * 440
+    
+    # for i in range(y_predict.shape[0]):
+    #     index_predict = np.argmax(y_predict[i, :])
+    #     pitch_MIDI = pitch_range[np.int32(index_predict)]
+    #     if pitch_MIDI >= 38 and pitch_MIDI <= 83:
+    #         est_pitch[i] = 2 ** ((pitch_MIDI - 69) / 12.) * 440
+
 
     est_pitch = medfilt(est_pitch, 5)
 
-    PATH_est_pitch = './results/pitch_'+file_name.split('/')[-1]+'.txt'
+    ''' save results '''
+
+    PATH_est_pitch = output_dir+'/pitch_'+filepath.split('/')[-1]+'.txt'
+
     if not os.path.exists(os.path.dirname(PATH_est_pitch)):
         os.makedirs(os.path.dirname(PATH_est_pitch))
     f = open(PATH_est_pitch, 'w')
@@ -77,10 +91,35 @@ def main(file_name):
         fig.tight_layout()
         plt.show()
 
-        # plt.savefig('test.pdf', bbox_inches='tight')
+
+def parser():
+    p = argparse.ArgumentParser()
+    p.add_argument('-fp', '--filepath',
+                   help='Path to input audio (default: %(default)s',
+                   type=str, default='train01.wav')
+    
+    p.add_argument('-gpu', '--gpu_index',
+                   help='Assign a gpu index for processing. It will run with cpu if None.  (default: %(default)s',
+                   type=int, default=None)
+    p.add_argument('-o', '--output_dir',
+                   help='Path to output folder (default: %(default)s',
+                   type=str, default='./results/')
+    return p.parse_args()
+
 
 if __name__ == '__main__':
     options = Options()
-    file_name = sys.argv[1]
-    main(file_name = file_name)
+    args = parser()
+    main(args.filepath, args.output_dir, args.gpu_index)
+
+
+# def JDC():    
+#     AudioPATH = './'  # ex) AudioPATH = './dataset/*.mp3'
+#     filePath = glob.glob(AudioPATH)
+#     for fileName in filePath:
+#         string = "python melodyExtraction_JDC.py "
+#         string += fileName
+#         os.system(string)
+
+
 
